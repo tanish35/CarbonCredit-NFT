b434af722e74a8a042726b3c01def6d74bcf36dc
tanish35 <tanishmajumdar2912@gmail.com>
2024-12-28 20:30:04 +0530

    Committing untracked files before installing OpenZeppelin contracts

---
+++ src/CarbonCreditsNFT.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract CarbonCreditsNFT is ERC721URIStorage, Ownable {
+    uint256 public tokenCounter;
+
+    constructor() ERC721("CarbonCreditsNFT", "CCNFT") {
+        tokenCounter = 0;
+    }
+
+    function mintNFT(address recipient, string memory tokenURI) public onlyOwner returns (uint256) {
+        uint256 newTokenId = tokenCounter;
+        _safeMint(recipient, newTokenId);
+        _setTokenURI(newTokenId, tokenURI);
+        tokenCounter += 1;
+        return newTokenId;
+    }
+}
