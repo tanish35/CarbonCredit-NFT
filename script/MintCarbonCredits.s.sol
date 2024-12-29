076752ea1c0e97731c3116548af98216d7667a55
tanish35 <tanishmajumdar2912@gmail.com>
2024-12-29 10:57:08 +0530

    Updated metadata

--- script/MintCarbonCredits.s.sol
+++ script/MintCarbonCredits.s.sol
@@ -7,7 +7,6 @@ import "../src/CarbonCreditsNFT.sol";
 contract MintCarbonCredits is Script {
     function run() external {
         vm.startBroadcast();
-
         CarbonCreditsNFT ccNFT = CarbonCreditsNFT(
             0x150Fa2411e99AC9D98E862496fc5E710b0aF63BB
         );
@@ -15,7 +14,7 @@ contract MintCarbonCredits is Script {
         address recipient = 0x424bBA2a6f1c14e4D8e2Cf1bCAE1B06740Fa4755;
 
         string
-            memory tokenURI = "https://blue-glamorous-spider-989.mypinata.cloud/ipfs/bafkreic4ly6zt3u5vteb5tylx4jcpiy6weycbuqxai6t557bvvxcwqjop4";
+            memory tokenURI = "ipfs://bafkreiab3ouagjt7rcpwa657aearptytdxeijjik6e7cmayvbcr3rdhhm4";
 
         ccNFT.mintNFT(recipient, tokenURI);
 
